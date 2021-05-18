150ee8c89a019cec8c54f5b3e7d5a0b7bb1d1e8a
Michal Wiraszka <michal@wiraszka.com>
2021-05-18 04:32:32 -0400

    navbar fully responsive; header styling changed; temp color scheme

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -8,14 +8,26 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
-  menuActive = false;
+  menuOpen = false;
 
   constructor(public auth: AngularFireAuth, private toastr: ToastrService) {}
+
+  closeDropdownMenu() {
+    this.menuOpen = false;
+    document.getElementById('top-nav')?.classList.remove('open');
+  }
+
+  toggleDropdownMenu() {
+    this.menuOpen = !this.menuOpen;
+    let topNav = document.getElementById('top-nav');
+    this.menuOpen ? topNav?.classList.add('open') : topNav?.classList.remove('open')
+  }
 
   onLogout() {
     this.auth.signOut();
     this.toastr.success('', 'Successfully logged out', {
       positionClass: 'toast-bottom-right'
     });
+    this.closeDropdownMenu();
   }
 }
