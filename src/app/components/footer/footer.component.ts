8ab3a3f8b46a17d56ac6325f47fce612ee5537c4
Michal Wiraszka <michal@wiraszka.com>
2021-06-05 14:52:33 -0400

    v0.6.0 branch created

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,7 +9,7 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
         &copy; 2021 Copyright: Michal Wiraszka
       </small>
       <small id="version" class="footer">
-        Currently in development (version 0.5.0)
+        Currently in development (version 0.6.0)
       </small>
       <address id="github-repo" class="bigger">
         <a href="https://github.com/mwiraszka/Chordbomb#readme" id="about-chordbomb">
